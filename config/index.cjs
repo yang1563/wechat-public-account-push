76a9d3cb18445a73fcc44fec895abb41d12f55ab
yang1563 <114561409+yang1563@users.noreply.github.com>
2022-09-28 19:42:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '金金',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oaQp_5ysgyRmBn8ThaQv1cR0QMeY',
       // 你想对他发送的模板消息的模板ID
@@ -82,7 +82,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '金金', year: '1996', date: '09-09',
         },
         {
           type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
@@ -227,7 +227,7 @@ const USER_CONFIG = {
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
     {
-      type: '*生日', name: '老婆', year: '1996', date: '09-09', isShowAge: true,
+      type: '*生日', name: '金金', year: '1996', date: '09-09', isShowAge: true,
     },
     {
       type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
