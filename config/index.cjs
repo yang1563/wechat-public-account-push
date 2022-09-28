a54fa114f3ff00ad50bdb3e37cc28fdbe6dcbf8e
yang1563 <114561409+yang1563@users.noreply.github.com>
2022-09-28 19:44:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -180,7 +180,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '4pfJCJfz0fguKOpGIStOGxMGr9LpYouXkAmvgPb7-hU',
+  CALLBACK_TEMPLATE_ID: '',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -188,7 +188,7 @@ const USER_CONFIG = {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oaQp_55I6fQ_ucbaKZ2i5fw83z7U',
+      id: '',
     }
     // 你可以不断按格式往下增加
     // ...
